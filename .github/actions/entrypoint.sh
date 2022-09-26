2538689f0833a544321eb88736ed807cc5e0c9df
Atolycs <dummy.github.1945+atolycs@gmail.com>
2022-09-26 22:05:26 +0900

    Fix Github actions

---
+++ .github/actions/entrypoint.sh
@@ -0,0 +1,15 @@
+#!/bin/sh -l
+
+if [ -d "/github" ];then
+    sudo chown -R build /github/workspace /github/home
+fi
+
+sudo pacman -Sy
+export MAKEFLAGS=-j$(nproc)
+namcap PKGBUILD
+makepkg -fC --syncdeps --noconfirm
+
+echo "================"
+echo "Package created:"
+echo `ls *.pkg.tar.zst`
+echo "================"
